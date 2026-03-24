badb33ce9faa8695be31b6c4a57ebe74e255f78e
Arnaud Roberto da Silva Júnior - PrestServ <arnaud.silva.prestserv@petrobras.com.br>
2026-03-23 21:40:12 -0300

    correção7

--- app.py
+++ app.py
@@ -77,3 +77,21 @@ def logout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
